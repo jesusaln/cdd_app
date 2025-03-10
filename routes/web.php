baf6c381c4595fbde28e9f4ee4b7aa3b82a5dbf2
Jesus Lopez Noriega <jesuslopeznoriega@hotmail.com>
2025-03-09 22:13:20 -0700

    1.9.1 REGRESE PARA REGISTRAR USUARIOS

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ use App\Http\Controllers\CitaController;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
