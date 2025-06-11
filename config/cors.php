76819163df46806e1d830ef314aa94966ca8f9e0
Jesus Lopez Noriega <jesuslopeznoriega@hotmail.com>
2025-06-10 18:33:52 -0700

    2.1.6 cors

--- config/cors.php
+++ config/cors.php
@@ -4,16 +4,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => [
-        'http://localhost:8100',
-        'capacitor://localhost',
-        'file://',
-    ],
-
-    'allowed_origins_patterns' => [
-        '^https?:\/\/.*\.climasdeldesierto\.laravel\.cloud$', // Acepta HTTP y HTTPS con subdominios
-        '^https?:\/\/climasdeldesierto\.laravel\.cloud$',     // También acepta el dominio raíz
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_headers' => ['*'],
 
