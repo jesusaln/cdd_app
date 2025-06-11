09f9e5fd2c0ad1d4487e861fab83702a80630ced
Jesus Lopez Noriega <jesuslopeznoriega@hotmail.com>
2025-06-10 18:07:25 -0700

    2.1.1 modificacion cors

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'], // Rutas protegidas por CORS
     'allowed_methods' => ['*'], // Métodos HTTP permitidos
-    'allowed_origins' => ['https://climasdeldesierto.laravel.cloud'], // Orígenes permitidos (tu frontend Ionic)
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'], // Encabezados permitidos
     'exposed_headers' => [],
     'max_age' => 0,
